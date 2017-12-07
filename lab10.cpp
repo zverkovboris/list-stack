5ccecd7add315527e9b990ccb4eb3da5b79960a8
zverkovboris <32186297+zverkovboris@users.noreply.github.com>
2017-12-07 21:34:59 +0300

    Update lab10.cpp

--- lab10.cpp
+++ lab10.cpp
@@ -1,1 +1,24 @@
 
+
+
+
+
+int main()
+{
+    ForwardList* list =initList();
+    pushFront(list,1);
+    pushFront(list,2);
+    pushFront(list,3);
+    pushFront(list,4);
+    pushFront(list,5);
+    popFront(list);
+    insertAfter(list,list->Head->Next,0);
+    eraseAfter(list, list->Head->Next->Next);
+    print(list);
+    std::cout << std::endl;
+    reverse(list);
+    print(list);
+    std::cout << size(list);
+    destroy(&list);
+    return 0;
+}
